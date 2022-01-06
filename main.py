2d585a5b266f8b171121022bb764703d46964102
AlexandraElena05 <56784887+AlexandraElena05@users.noreply.github.com>
2022-01-06 23:50:46 +0200

    scrollbar + get searched topic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import tkinter as tk
+from tkinter import *
+
 
 def getNews():
     api_key = "c89a0fdb7dc24da8ba6cbc15c6152dad"
@@ -9,16 +11,29 @@ def getNews():
     class Article:
         title = ""
         content = ""
-        def __init__(self,title,content):
-            self.title=title
-            self.content=content
 
+        def __init__(self, title, content):
+            self.title = title
+            self.content = content
 
     articles = news["articles"]
 
     my_articles = []
     my_news = ""
-    searched_topic = "Democrats"
+
+    def search():
+        searched_topics = enter_box.get()
+        return searched_topics
+
+    label1 = tk.Label(canvas, text = "Enter your search here", font = 15)
+    label1.grid(row=0, column=0)
+    enter_box = tk.Entry(canvas, width = 35)
+    enter_box.grid(row=0, column=1)
+    btn = tk.Button(canvas, text = "Search", command = search, bg = "blue")
+    btn.grid(row=1, column=0)
+
+    searched_topic = search()
+
     for article in articles:
         if(article["content"].find(searched_topic)!=-1):
             my_articles.append(Article(article["title"], article["content"]))
@@ -28,14 +43,19 @@ def getNews():
 
     label.config(text=my_news, wraplength=550)
 
+
 canvas = tk.Tk()
 canvas.geometry("900x600")
 canvas.title("GUI News Feed")
 label = tk.Label()
 
+scrollbar = tk.Scrollbar(canvas)
+scrollbar.pack(side=RIGHT, fill=Y)
+
 menu = tk.Menu(canvas)
 canvas.config(menu=menu)
 file_menu = tk.Menu(menu)
+
 
 file_menu = tk.Menu(
     menu,
@@ -66,6 +86,7 @@ menu.add_cascade(
     menu=help_menu
 )
 
+
 def myClick():
     getNews()
     myLabel = tk.Label(canvas, font=18, text="Now the news are refreshed!")
@@ -76,6 +97,7 @@ button.pack(pady=20)
 
 label = tk.Label(canvas, font=18, justify="left")
 label.pack(pady=20)
+
 
 getNews()
 
